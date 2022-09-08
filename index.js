e26a9c178b51054c9167e5109d230979c3b694ce
DenisOuma <dennisouma38@gmail.com>
2022-09-08 11:08:11 +0300

    Assignment complete

--- index.js
+++ index.js
@@ -1,1 +1,40 @@
-// Your code here
+// Your code here!
+class Person {
+	constructor(name, age) {
+		this.name = name;
+		this.age = age;
+	}
+	speak() {
+		console.log(`${this.name} says hello`);
+	}
+}
+
+class Cat extends Person {
+	constructor(name, age) {
+		super(name, age);
+	}
+	speak() {
+		return `${this.name} says meow!`;
+	}
+}
+
+class Dog extends Person {
+	constructor(name, age) {
+		super(name, age);
+	}
+	speak() {
+		return `${this.name} says woof!`;
+	}
+}
+
+class Bird extends Person {
+	constructor(name, age) {
+		super(name, age);
+	}
+	speak() {
+		return `It's me! ${this.name}, the parrot!`;
+	}
+	speak() {
+		return `${this.name} says squawk!`;
+	}
+}
